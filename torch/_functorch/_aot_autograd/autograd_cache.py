50f843317b462f169e780a548ba8c3178ea25a33
James Wu <jjwu@meta.com>
2024-05-21 09:04:27 -0700

    Add basic sanity checks for graph ops to cache key
    ghstack-source-id: 515ab4f7017f0bc12c8717d3c490d2cefc288371
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124745

--- torch/_functorch/_aot_autograd/autograd_cache.py
+++ torch/_functorch/_aot_autograd/autograd_cache.py
@@ -3,17 +3,77 @@ Utils for caching the outputs of AOTAutograd
 """
 from __future__ import annotations
 
+import functools
 import logging
-from typing import TYPE_CHECKING
+import os
 
-from torch._inductor.codecache import _ident, FxGraphCachePickler
+import torch
+from torch._inductor.codecache import (
+    _ident,
+    FxGraphCachePickler,
+    get_code_hash,
+    get_inductor_root,
+)
+from torch.fx.node import Node
 
 from .schemas import AOTConfig  # noqa: F401
 
-if TYPE_CHECKING:
-    import torch
-
 log = logging.getLogger(__name__)
+
+
+class BypassAOTAutogradCache(Exception):
+    pass
+
+
+def check_node_safe(node: Node):
+    """
+    Checks that the node only uses supported operators.
+    """
+
+    def is_torch_function(target):
+        return type(target).__name__.startswith("builtin")
+
+    def is_tensor(target):
+        # Tensors always have example values in meta field
+        return "example_value" in target.meta
+
+    # I'd love to use a match statement here, but it wasn't introduced until py3.10
+    if node.op == "call_function":
+        # We support only torch.* functions for now
+        # We can probably add an allowlist of safe non-torch implementations as well
+        if not is_torch_function(node.target):
+            raise BypassAOTAutogradCache(
+                f"Unsupported call_function target {node.target}"
+            )
+    elif node.op == "call_method":
+        method_target = node.args[0]
+        if not is_tensor(method_target):
+            # We support only method calls on tensors and symints
+            raise BypassAOTAutogradCache(
+                f"Unsupported call_method target {method_target}"
+            )
+    # Cache safe
+    elif node.op in ("placeholder", "call_module", "get_attr", "output"):
+        # TODO: not all call_modules may be safe
+        pass
+    else:
+        raise BypassAOTAutogradCache(f"Unsupported node op {node.op}")
+
+
+@functools.lru_cache(None)
+def get_autograd_code_hash():
+    autograd_root = os.path.dirname(__file__)
+    inductor_root = get_inductor_root()
+    return get_code_hash([autograd_root, inductor_root])
+
+
+def check_cacheable(gm: torch.fx.GraphModule):
+    """
+    Checks that the graph module only uses supported operators
+    """
+    nodes = gm.graph.nodes
+    for node in nodes:
+        check_node_safe(node)
 
 
 class AOTAutogradCacheDetails:
@@ -26,6 +86,8 @@ class AOTAutogradCacheDetails:
         self.gm = gm  # TODO: we'll handle different parts of the graph module
         # TODO: We'll want to handle the full_args passed in as well
         self.config = config  # Gets reduced by the Pickler
+        check_cacheable(gm)
+        self.code_hash = get_autograd_code_hash()
 
     def debug_str(self) -> str:
         return AOTAutogradCachePickler.debug_str(self)
